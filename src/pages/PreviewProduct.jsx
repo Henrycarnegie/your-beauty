f487c22bb86a2dfb9826638bed78b2c94572c618
LAPTOP-L6GF21A1\Lenovo <89569018+SamuelSiagian835@users.noreply.github.com>
2024-05-19 17:20:47 +0700

    Struct

---
+++ src/pages/PreviewProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PreviewProduct = () => {
+//   return (
+//     // <div>
+//     //   <h1>Preview Product</h1>
+//     //   <p>Check out our product previews.</p>
+//     // </div>
+//   );
+};
+
+export default PreviewProduct;
---
+++ src/pages/Profil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profil = () => {
+//   return (
+//     <div>
+//       <h1>Profil</h1>
+//       <p>View your profile information here.</p>
+//     </div>
+//   );
+};
+
+export default Profil;
